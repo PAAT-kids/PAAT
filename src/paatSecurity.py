3e0dfb17ecd17ea80d3d20c3a53872d03c043288
MajidJafar <90322535+MajidJafar@users.noreply.github.com>
2021-09-22 18:14:53 +0400

    Password Hashing , Task 3
    Added code comments and docstrings

--- src/paatSecurity.py
+++ src/paatSecurity.py
@@ -1,11 +1,32 @@
+"""
+FILENAME: paatSecurity
+AUTHOR: Majid Jafar
+PURPOSE: Contains all the important security asoect of the program
+DATE CREATED: 22/09/2021
+LAST EDITED DATE: 22/09/2021
+"""
 import bcrypt
 #run "pip install bcrypt" in terminal
 
+
+"""
+FUNCTION NAME: passHash
+PURPOSE: Hashes the imported string value
+INPUT: String password
+OUTPUT: Bit String hashPassword
+"""
 def passHash (password):
     passwordBits = password.encode("utf-8")
     hashedPassword = bcrypt.hashpw(passwordBits, bcrypt.gensalt())
     return hashedPassword
 
+
+"""
+FUNCTION NAME: compPass
+PURPOSE: Compares the given input to the hashvalue from the database
+INPUT: String password , Bit String dbPassword
+OUTPUT: Boolean
+"""
 def compPass (password,dbPassword):
     passwordBits = password.encode("utf-8")
     
