4dcbc50a81582d023079dbdd87f4d7cad5d64eab
ahmedlarbi <hamood2002011@hotmail.com>
2021-10-08 22:30:43 +0400

    #4            Added SendUDP Function and Send Ethernet Function

--- src/drafting.py
+++ src/drafting.py
@@ -3,7 +3,7 @@ import sys
 
 
 srcip = ""
-dstip = "192.1.168.1"
+dstip = ""
 totallength = 60
 len = ""
 version = ""
@@ -16,6 +16,29 @@ protocol = ""
 cheksum = ""
 options = ""
 
-scapy.sr(scapy.IP(dst=dstip, ttl = totallength)/scapy.ICMP())
+def setVariables(srcip1,dstip1,totallength1,len1,version1,ihl1,tos1,id1,flgs1,frag1,protocol1,cheksum1,options1):
+    srcip = srcip1
+    dstip = dstip1
+    totallength = totallength1
+    len = len1
+    version = version1
+    ihl = ihl1
+    tos = tos1
+    id = id1
+    flgs = flgs1
+    frag = frag1
+    protocol = protocol1
+    cheksum = cheksum1
+    options = options1
+
+def sendTCP():
+    scapy.sr(scapy.IP(dst=dstip, ttl = totallength)/scapy.TCP_SERVICES())
+
+def sendEther():
+    scapy.sr(scapy.IP(dst=dstip, ttl = totallength)/scapy.Ether())
+
+
+
+
 
 
