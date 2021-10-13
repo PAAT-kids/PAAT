38481e268329f244f0cc00ea287daf355e695fd6
MajidJafar <majidjafarali@gmail.com>
2021-10-13 14:09:57 +0400

    Craft Packets#2  Completed NTP packet crafting and sending

--- src/sendPacket.py
+++ src/sendPacket.py
@@ -5,9 +5,32 @@ import scapy.all as scapy
 def sendPacket(type,ethSrc,ethDst,ethType,ipVersion,ipIhl,ipTos,ipLen,ipId,ipFlags,ipFrag,ipTtl,ipProto,ipChksum,ipSrc,ipDst,ipOptions,udpSport,udpDport,udpChksum,listValues):
     #Ethernet Variables ethSrc ,ethDst
 
-    packet = (scapy.Ether(src=ethSrc,dst=ethDst,type=ethType)
-                /scapy.IP(version=ipVersion,ihl=ipIhl,tos=ipTos,len=ipLen,id=ipId,flags=ipFlags,frag=ipFrag,ttl=ipTtl,proto=ipProto,chksum=ipChksum,src=ipSrc,dst=ipDst,options=ipOptions)
-                /scapy.UDP(sport=udpSport,dport=udpDport,chksum=udpChksum))
+    packet = (scapy.Ether(
+                        src=ethSrc,
+                        dst=ethDst,
+                        type=ethType
+                        )
+                /scapy.IP(
+                        version=ipVersion,
+                        ihl=ipIhl,
+                        tos=ipTos,
+                        len=ipLen,
+                        id=ipId,
+                        flags=ipFlags,
+                        frag=ipFrag,
+                        ttl=ipTtl,
+                        proto=ipProto,
+                        chksum=ipChksum,
+                        src=ipSrc,
+                        dst=ipDst,
+                        options=ipOptions
+                        )
+                /scapy.UDP(
+                        sport=udpSport,
+                        dport=udpDport,
+                        chksum=udpChksum
+                        )
+                    )
 
     if(type == 1):
         sendPacketDNS(packet,listValues)
@@ -20,11 +43,40 @@ def sendPacket(type,ethSrc,ethDst,ethType,ipVersion,ipIhl,ipTos,ipLen,ipId,ipFla
 
 def sendPacketDNS(packet,listValues):
 
-    packet = packet/scapy.DNS(rd=listValues[0],qd=listValues[1])
+    packet = packet/scapy.DNS(  rd=1,
+                                qd=scapy.DNSQR(
+                                        qname=listValues[0],
+                                        qtype=listValues[1],
+                                        qclass=listValues[2]
+                                        )
+                              )
+
+    scapy.send(packet)
 
 
+def sendPacketNTP(packet,listValues):
 
-#def sendPacketNTP(packet,listValues):
+    packet = packet/scapy.NTPHeader(
+                                        leap=listValues[0],
+                                        version=listValues[1],
+                                        mode=listValues[2],
+                                        stratum=listValues[3],
+                                        poll=listValues[4],
+                                        precision=listValues[5],
+                                        delay=listValues[6],
+                                        dispersion=listValues[7],
+                                        id=listValues[8],
+                                        ref_id=listValues[9],
+                                        ref=listValues[10],
+                                        orig=listValues[11],
+                                        recv=listValues[12],
+                                        sent=listValues[13]
+                                        
+                                        )
+
+    scapy.send(packet)
+
+                                    
 
 
 #def sendPacketSSDP(packet,listValues):
