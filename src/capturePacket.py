7764825aae990683841b4b891406d0b95a688a83
Samrah <63675360+samrah-tahir@users.noreply.github.com>
2021-10-28 10:29:05 +0400

    improved capture packet feature

--- src/capturePacket.py
+++ src/capturePacket.py
@@ -1,129 +1,139 @@
-from scapy.all import *
+from scapy.all import sniff, Raw, UDP,DNSQR,DNSRR
 
-'''
-#Author: Samrah Tahir				 			              
-#LAST MODIFIED: 16 Oct 2021			  			          
-#PURPOSE: To capture the packets and calculate 	  
-#amplificaiton 				           	 		            
-'''
-DNSQuery = {"src": '', "dst": '', "sport": ''} #temporary data structures
-NTPQuery = {"src": '', "dst": '', "mode": 0}
-NTPQueryList = []
+recvIP = "127.0.0.1" #replace with host comps IP
+srcIP = ""
+dnsIP = "8.8.8.8" #replace with our own dns server
+def checkSelectedOption():
+    rawPacket=''
+    #option = sniff(stop_filter= lambda x: x.haslayer(Raw))
+    
+    while(True):
+       
+        option = sniff(filter = "udp and dst host 1.2.3.4", stop_filter= lambda x: x.haslayer(Raw))
+        print("outside sniff that was in while.")
+        for rawPacket in option[UDP]:
+            if rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'DNS':
+                 break #breaking for loop
+	    elif rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'NTP':
+		 break
+	    elif rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'SSDP':
+		 break
+        if rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'DNS':
+            print("found dns")
+            printDNSVal(rawPacket) #printDNSValues
+            break #breaking while loop
+	elif rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'NTP':
+            print("found ntp")
+            #printDNSVal(rawPacket) #printDNSValues
+            break #breaking while loop
+	elif rawPacket.haslayer(Raw) and rawPacket.getlayer(Raw).load == 'SSDP':
+            print("found ssdp")
+            printSSDPVal(rawPacket) #printDNSValues
+            break #breaking while loop
+        print("continuing while loop")
+        
+    
+    
+    #print(option[0][Raw].load)
 
-'''
-Purpose: captures packets on all interfaces
-Input: None
-Output: capturedPacket, 1 when unsuccessful
-'''
-def capturePackets():
-	capturedPacket = sniff(stop_filter= lambda pkt: testLayer(pkt))
+#ssdp packet handle
+#sport for ssdp query is 65165 and dport is ofc 1900
+def printSSDPVal(ssdpPacket):
+	ssdpQPacket = None
+	ssdpAnsPacket = []
+	#various packets may be captured in one sniff
+	ssdpPackets = sniff(filter="udp and port 65165", timeout=5) #sniffs for at most 5 secs
 	
-	if len(capturedPacket) == 0:
-		return 1
-'''
-	for packet in capturedPacket:
-		if packet.haslayer(DNS):
-			handleDNS(packet)
-		elif packet.haslayer(NTPHeader):
-			handleNTP(packet)
+	for ssdpPacket in ssdpPackets[UDP]:
+		if ssdpPacket.sport == 65165 and ssdpPacket.dport == 1900:
+			ssdpQPacket = ssdpPacket#should be the ssdp Q packet
+		elif ssdpPacket.dport == 65165 and ssdpPacket.sport == 1900:
+			ssdpAnsPacket.append(ssdpPacket)#should be the ssdp Ans packets
 
-'''
-
-'''
-Purpose: calculate ssdp packet length
-Input: packet
-Output: none
-'''
-
-def handleSSDP(packet):
-	if packet.haslayer(UDP) and packet[UDP].dport == 1900 and packet.haslayer(Raw):
-		ssdpQueryLen = len(packet[Raw])
-	elif packet.haslayer(UDP) and packet[UDP].sport == 1900 and packet.haslayer(Raw):
-		ssdpRespLen = len(packet[Raw])
-	
-	ampFactor = calculateAmpFactor(ssdpQueryLen, ssdpRespLen)
-	print("ssdp ampfactor: ", ampFactor)
-
-'''
-Purpose: Comparing query packet and response packet
-to calculate amplification
-Input: packet
-Output: none
-'''
-def handleDNS(packet):
-	if packet.haslayer(DNS) and packet.haslayer(DNSQR) and not packet.haslayer(DNSRR):
-		DNSQuery["src"] = packet[IP].src
-		DNSQuery["dst"] = packet[IP].dst
-		DNSQuery["len"] = packet[UDP].len
-		DNSQuery["sport"] = packet[UDP].sport
-   	elif packet.haslayer(DNS) and packet.haslayer(DNSRR):
-		if packet[IP].dst == DNSQuery["src"] and packet[IP].src == DNSQuery["dst"] and packet[IP].dport == DNSQuery["sport"]:
-			print("Amplification factor is ", calculateAmpFactor(DNSQuery["len"], getLength(packet)))
-		#print("OG: ",DNSQuery["len"])#len w udp header 
-			#print("resp ",getLength(packet))#len w udp header
-
-'''
-Purpose: calculates amplification percentage
-Input: origLength, resLength
-Output: ampFactor(%)
-'''
-def calculateAmpFactor(origLength, resLength):
-	ampFactor = ((resLength - origLength)/origLength)*100
-	return ampFactor
-
-'''
-Purpose: handles NTP packet
-Input: packet
-Output: None
-'''
-def handleNTP(packet):
-	#if it is NTP and it a request from client
-	if packet.haslayer(NTP) and packet[NTPHeader].mode == 3:
-		NTPQuery["src"] = packet[IP].src
-		NTPQuery["dst"] = packet[IP].dst
-		NTPQuery["mode"] = packet[NTPHeader].mode
-		NTPQuery["len"] = getLength(packet)
-		NTPQueryList.append(NTPQuery)
 		
-	elif packet.haslayer(NTP) and packet[NTPHeader].mode == 4:
-		if NTPQueryList[0]["src"] == packet[IP].dst:
-			print(getLength(packet))
+	if ssdpQPacket and ssdpAnsPacket:
+		print("Length of SSDP Q Packet is: "+str(dnsQAlengthCal(ssdpQPacket)))
+		print("total length of ssdp Ans packets is: "+str(ssdpAlengthCal(ssdpAnsPacket)))
+		print("Amplification :"+str(dnsAmplificationFactor(dnsQAlengthCal(ssdpQPacket), ssdpAlengthCal(ssdpAnsPacket))))
 
+#calculate length of ssdp responses in total
+def ssdpAlengthCal(ssdpPacket):
+	total = 0
+	for ssdpPkt in ssdpPacket:
+		total = total + dnsQAlengthCal(ssdpPkt)
+	return total
 '''
-Purpose: calculate length of NTP and DNS packets
-(including UDP header bytes)
-Input: pkt(packet)
-Output: pkt[UDP].len(length of packet)
+NTP notes: can know whether its a query or answer by looking at the sport and dport
 '''
-def getLength(pkt):
-	return pkt[UDP].len
+def printNTPVal(ntpPacket):
+	ntpQPacket = None
+	ntpAnsPacket = None
 
-'''
-Purpose: testing the type of packet it is
-Input: pkt(packet)
-Output: None
-'''
-def testLayer(pkt):
+	print("inside printNTPVal")
+	
+	
+#for printing dns values
+def printDNSVal(dnsPacket):
+    dnsQPacket = None
+    dnsAnsPacket = None
+    print("inside printDNSVal")
+    dnsPackets = sniff(filter = "host "+dnsIP, prn= lambda x: x.haslayer(DNSQR) or x.haslayer(DNSRR))
+    print("dnsqpacket stopped sniffing.")
+    for dnsPacket in dnsPackets[UDP]:
+        if dnsPacket.haslayer(DNSQR) and dnsPacket.getlayer(UDP).dport==53:
+            dnsQPacket = dnsPacket #getting the dns query packet
+        elif dnsPacket.haslayer(DNSRR) and dnsPacket.getlayer(UDP).sport==53:
+            dnsAnsPacket = dnsPacket#getting the dns response record packet
+    #dnsAnsPacket = sniff(filter = "dst host 192.168.231.173", prn= lambda x: x.haslayer(DNSRR), timeout=5,quiet=True)
+    if dnsQPacket and dnsAnsPacket:
 
-	if pkt.haslayer(DNS):
-		handleDNS(pkt)
-	elif pkt.haslayer(NTP):
-		print("ntp")
-		handleNTP(pkt)
-	elif pkt.haslayer(UDP):
-		if pkt[UDP].dport ==1900 or pkt[UDP].sport == 1900:
-			handleSSDP(pkt)
+        #functions for dnsq length calculations
+        print("dnsQ length: "+str(dnsQAlengthCal(dnsQPacket)))
+        #functions for dnsa length calculations
+        print("dnsAns length: "+str(dnsQAlengthCal(dnsAnsPacket)))
+        #function to calculate the amplification
+        dnsAmplificationFactor(dnsQAlengthCal(dnsQPacket),dnsQAlengthCal(dnsAnsPacket))
+
+        print("dnsQPacket: ")
+        print(repr(dnsQPacket))
+        print("dnsAnsPacket: ")
+        print(repr(dnsAnsPacket))
+
+#calculate length of dnsq
+def dnsQAlengthCal(dnsQPkt):
+    dnsQPacketLength = dnsQPkt.getlayer(UDP).len
+    return dnsQPacketLength
+
+#calculate dns amplification
+def dnsAmplificationFactor(qLength, rlength):
+    ampFactor = (float((rlength - qLength))/float(qLength))*100
+    print("***The Amplification for dns is****"+str(ampFactor)+"%")
+
+
+
+
+
+
+
+
+
+
+
+
+def checkLayer(pkt):
+    if(pkt.haslayer(Raw) and pkt[0][Raw].load == 'DNS'):
+        sniffDNSPackets()
+        #print(pkt[0][Raw].load)
+
+        return 1
+
+
+def sniffDNSPackets():
+    print("1")
+    sniff(filter = "udp and host "+recvIP, prn=extractDNSInfo)
    
 
-if __name__ == '__main__':
-	if capturePackets() == 1:
-		print("No packets captured.")
-
-#def testFunc(pkt):
-#	print(repr(pkt))
-
-#print(len(capturedPacket))
-#print(repr(capturedPacket))
-
-
-
+def extractDNSInfo():
+     print("2")
+     print("sniffing packets")
+checkSelectedOption()
