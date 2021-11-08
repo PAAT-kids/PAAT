69640e14c9547dfaeb48b6dd4ac3c20f104eadee
Samrah Tahir <samrah.tahir@student.curtin.edu.au>
2021-11-08 19:07:53 +0400

    Added functions for displaying received values

--- src/receiver.py
+++ src/receiver.py
@@ -17,36 +17,38 @@ from PyQt5.QtWidgets import *
 '''
 Purpose: display received packet info in received log
 '''
-def displayReceiveLog(self):
+# def displayReceiveLog(self):
 	
-	rowPosition = self.tableRecvd.rowCount()
-	self.tableRecvd.insertRow(rowPosition)
-	self.tableRecvd.setItem(rowPosition, 0, QTableWidgetItem(15))
-	self.tableRecvd.setItem(rowPosition, 1, QTableWidgetItem("monday"))
-	self.tableRecvd.setItem(rowPosition, 2, QTableWidgetItem(17))
-	self.tableRecvd.setItem(rowPosition, 3, QTableWidgetItem(18))
-	self.tableRecvd.setItem(rowPosition, 4, QTableWidgetItem(120))
-'''
+# 	rowPosition = self.tableRecvd.rowCount()
+# 	self.tableRecvd.insertRow(rowPosition)
+# 	self.tableRecvd.setItem(rowPosition, 0, QTableWidgetItem(15))
+# 	self.tableRecvd.setItem(rowPosition, 1, QTableWidgetItem("monday"))
+# 	self.tableRecvd.setItem(rowPosition, 2, QTableWidgetItem(17))
+# 	self.tableRecvd.setItem(rowPosition, 3, QTableWidgetItem(18))
+# 	self.tableRecvd.setItem(rowPosition, 4, QTableWidgetItem(120))
+
 def displayReceiveLog(self):
         cnx = mysql.connector.connect(user='me', password='myUserpassword',host='127.0.0.1',database='PAAT')
         cursor = cnx.cursor()
         self.tableRecvd.setRowCount(0) #refreshing the table each time the receive log is viewed
-        query = ("SELECT Sizee, ReceiverAdd, FinalSize, Amplification FROM Received"
-                 )
-        cursor.execute(query)
+        #select Sizee,Datee, ReceiverAdd, FinalSize,Amplification FROM Received JOIN Receives on Received.ID = Receives.ID JOIN Users on Users.Username = Receives.username
+        query = ("SELECT Sizee, Datee,ReceiverAdd, FinalSize, Amplification FROM Received"
+                 " JOIN Receives on Received.ID = Receives.ID JOIN Users on Users.Username = Receives.username")
 
-        for (initSize, recAddr,respSize, ampFactor) in cursor:
+        cursor.execute(query)
+        print(cursor)
+        for (initSize, dateRec,recAddr,respSize, ampFactor) in cursor:
 
             rowPosition = self.tableRecvd.rowCount()
             self.tableRecvd.insertRow(rowPosition)
             self.tableRecvd.setItem(rowPosition, 0, QTableWidgetItem(recAddr))
-            self.tableRecvd.setItem(rowPosition, 1, QTableWidgetItem("monday"))
+            self.tableRecvd.setItem(rowPosition, 1, QTableWidgetItem(dateRec.strftime('%Y-%m-%d')))
             self.tableRecvd.setItem(rowPosition, 2, QTableWidgetItem(str(initSize)))
             self.tableRecvd.setItem(rowPosition, 3, QTableWidgetItem(str(respSize)))
             self.tableRecvd.setItem(rowPosition, 4, QTableWidgetItem(str(ampFactor)))
         cursor.close()
         cnx.close()
-'''
+
 #sniff sniff
 def startSniffing():
     t1 = threading.Thread(target=startSniff, args=(10,))
