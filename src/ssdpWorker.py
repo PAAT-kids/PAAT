8afa53e148864512d8b4e4ed0059aca648560c62
Samrah Tahir <samrah.tahir@student.curtin.edu.au>
2021-11-25 17:16:36 +0400

    Worker class for thread functioning

--- src/ssdpWorker.py
+++ src/ssdpWorker.py
@@ -1,13 +1,21 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 from ssdpAuto import startSsdpGA
 
 class WorkerThread(QThread):
-    def __init__(self,message):
+    update_progress = pyqtSignal(str)
+
+    def __init__(self, source):
         super(QThread, self).__init__()
-        self.message = message
+        self.source = source
         #self.chosen = chosenPkt
 
     def run(self):
         #if self.chosen == 'SSDP':
-        startSsdpGA(self.message)
+        if self.source:
+            self.update_progress.emit("<h1>SSDP</h1>SSDP Random Selection Has Started")
+            result = startSsdpGA(self.source)
+            self.update_progress.emit(result)
+        else:
+            self.update_progress.emit('<div> <h1>Error</h1>' +'<p> No source Address specified! </p>'+'</div>')
+           
