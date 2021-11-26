98bf9bfac5aa7aa808aae4e816a68300085bfc48
majidjafar <majidjafarali@gmail.com>
2021-11-26 22:10:30 +0400

    Auto Create#1 Completed and linked DNS auto create to the UI

---
+++ src/dnsWorker.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QThread, pyqtSignal
+from dnsAuto import startDNSAuto
+
+class WorkerThreadDns(QThread):
+    update_progress = pyqtSignal(str)
+
+    def __init__(self, source):
+        super(QThread, self).__init__()
+        self.source = source
+        #self.chosen = chosenPkt
+
+    def run(self):
+        if self.source:
+            self.update_progress.emit("<h1>DNS</h1>DNS Random Selection Has Started")
+            result = startDNSAuto(self.source)
+            self.update_progress.emit(result)
+        else:
+            self.update_progress.emit('<div> <h1>Error</h1>' +'<p> No source Address specified! </p>'+'</div>')
+           
