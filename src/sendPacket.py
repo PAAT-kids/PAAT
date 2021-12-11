a2d4e4081bc09ac442e3fd9bc806c5c7a20435d6
ahmedlarbi <90322766+ahmedlarbi@users.noreply.github.com>
2021-12-11 17:56:58 +0400

    Fixed Errors in testing.

--- src/sendPacket.py
+++ src/sendPacket.py
@@ -351,6 +351,16 @@ class sendPacketClass:
                                 return (responses[1][scapy.UDP].len)
         else:
             return 0
+        
+        
+    def autoSendNTP(self,packet):
+       response, unans= scapy.sr(packet,timeout=1,verbose=0,multi=True)
+
+       if len(response[scapy.UDP]) != 0:
+           for responses in response[scapy.UDP]:
+                               return (responses[1][scapy.UDP].len)
+       else:
+           return 0
 
     def sizePacket(self,Type,QID, size, dest):
         t = '\"Type\"'
